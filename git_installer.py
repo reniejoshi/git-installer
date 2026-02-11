5e9f2dc2a0ca0875a13bd7f63262a760e7446dae
reniejoshi <renie_joshi@yahoo.com>
2026-02-11 10:01:32 -0800

    Practice GET request with quotes API

--- git_installer.py
+++ git_installer.py
@@ -1,10 +1,17 @@
 import os
+import requests
 import time
 from tqdm import tqdm
 
 os.system("echo Hello, world!")
 
 def main():
+    response = requests.get("https://dummyjson.com/quotes")
+    data = response.json()
+    quote = data["quotes"][0]
+    print('\"' + quote["quote"] + '\"')
+    print(quote["author"])
+
     for i in tqdm(range(10000)):
         time.sleep(0.0001)
 
